33d32e54afe4744fe261dc681cd2de648057074f
Oliviero Taleghani <o.taleghani@talesign.com>
2024-05-25 18:55:01 +0200

    feat: oauth get refresh and access token 3

--- internal/oauth2/token_access.go
+++ internal/oauth2/token_access.go
@@ -12,12 +12,12 @@ func GetAccessToken(clientID, clientSecret, refreshToken string) (string, error)
 	// Create a token with the refresh token.
 	// token := &oauth2.Token{RefreshToken: refreshToken}
   config := &oauth2.Config{
-    ClientID:     "1029956480466-f2p5kkp4dsahbtrh0q1svm0mvuu3dsja.apps.googleusercontent.com",
-		ClientSecret: "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",
+    ClientID:     "",
+		ClientSecret: "",
 		Endpoint:    google.Endpoint,
   }
   token := &oauth2.Token{
-    RefreshToken: "1//09-lyTNr5WI27CgYIARAAGAkSNwF-L9IrOme_pyxAHCPTEMriKYxg4YJ5141xQHmI9fnT3TPg300rYBzhb5V0d8L-ohwTZfFz1wk",
+    RefreshToken: "",
   }
 
 	// Create a token source with the refresh token.
